955a57bab79436d8699df502831a37861917431a
Egor <khromove@gmail.com>
2020-05-17 12:49:13 +0700

    add some logic to player

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -3,13 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start () {
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
 
+    public int currentHealth = maxHealth;
+    public bool isEnemy = false;
+    public RectTransform healthBar;
+
+    private bool isLocalPlayer;
+
+    // Start is called before the first frame update
+    void Start() {
+        PlayerController pc = GetComponent<PlayerController>();
+        isLocalPlayer = pc.isLocalPlayer;
+    }
+
+    public void TakeDamage(GameObject playerFrom, int amount) {
+        currentHealth -= amount;
+        //TODO network
+    }
+
+    public void onChangeHealth() {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+
+        if (currentHealth <= 0) {
+            if (destroyOnDeath) {
+                Destroy(gameObject);
+            } else {
+                currentHealth = maxHealth;
+                healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+                Respown();
+            }
+        }
+    }
+
+    void Respown() {
+        if (isLocalPlayer) {
+            Vector3 spawnPoint = Vector3.zero;
+            Quaternion spawnRotation = Quaternion.Euler(0, 180, 0);
+            transform.position = spawnPoint;
+            transform.rotation = spawnRotation;
+        }
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
 
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public GameObject bulletPrefab;
-    public Transform bullet;
+    public Transform bulletSpawn;
+    public bool isLocalPlayer = true;
+
+    Vector3 oldPosition;
+    Vector3 currentPosition;
+    Quaternion oldRotation;
+    Quaternion currentRotation;
 
     // Start is called before the first frame update
-    void Start () {
-
+    void Start() {
+        oldPosition = transform.position;
+        currentPosition = oldPosition;
+        oldRotation = transform.rotation;
+        currentRotation = oldRotation;
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
+        if (!isLocalPlayer) return;
 
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, z);
+        transform.Translate(0, 0, z);
+
+        currentPosition = transform.position;
+        currentRotation = transform.rotation;
+
+        if (currentPosition != oldPosition) {
+            oldPosition = currentPosition;
+        }
+
+        if (currentRotation != oldRotation) {
+            oldRotation = currentRotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            // new.Comm
+            CmdFire();
+        }
+    }
+
+    public void CmdFire() {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        Bullet b = bullet.GetComponent<Bullet>();
+        // b.playerFrom = this.gameObject;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6;
+
+        Destroy(bullet, 2.0f);
     }
 }
